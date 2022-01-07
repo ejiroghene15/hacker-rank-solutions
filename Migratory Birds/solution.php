22f63adfe43abbd55145c42abc0f61be6bb62e8d
ejiroghene <ejiroghene15@gmail.com>
2022-01-07 11:15:48 +0100

    migratory birds solution

--- Migratory Birds/solution.php
+++ Migratory Birds/solution.php
@@ -1,5 +1,4 @@
 <?php
-// $birds_array =  [1, "hello", 1, "world", "hello", 1, 'world', 'world', 'world'];
 
 #1 Read contents from file
 $file = file_get_contents('./birds_array.php');
@@ -13,6 +12,5 @@ sort($birds_array);
 #4 Use the array_count_values function to get the number of duplicate values of each element in the array
 $count_birds = array_count_values($birds_array);
 
-// print_r($count_birds);
 #5 Return the key of the element in array which appears most
 echo array_search(max($count_birds), $count_birds);
