c0c47a547df1749be9cb41ba777a00de18dfb184
ejiroghene <ejiroghene15@gmail.com>
2022-01-18 21:42:07 +0100

    Refactored Solution to the equalize array problem

--- Equalize Array/solution.php
+++ Equalize Array/solution.php
@@ -1,6 +1,13 @@
 <?php
+function equalizeArray($arr)
+{
+	$arr_length = count($arr);
+	$max_value_count = max(array_count_values($arr));
+	$min_deletion = $arr_length - $max_value_count;
+	return $min_deletion;
+}
+
+// * Test Input
 $arr =  [3, 3, 2, 1, 3];
-$arr_length = count($arr);
-$max_value_count = max(array_count_values($arr));
-$min_deletion = $arr_length - $max_value_count;
-return $min_deletion;
+
+echo equalizeArray($arr);
